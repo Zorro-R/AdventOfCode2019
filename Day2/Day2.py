c7cad8e59309a420a58f90a4fb3aa5abe91dfa53
Jurij Waite <jurijlwaite@gmail.com>
2019-12-07 18:14:18 +0100

    Added solution for day 2 part 2

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -1,24 +1,38 @@
-def run_intcode(intcode):
-    for i, code in enumerate(intcode):
-        # Is code an operation
-        if i % 4 == 0:
-            # Check operation type
-            if code == 1:
-                intcode[intcode[i+3]] = intcode[intcode[i+1]] + \
-                    intcode[intcode[i+2]]
-            elif code == 2:
-                intcode[intcode[i+3]] = intcode[intcode[i+1]] * \
-                    intcode[intcode[i+2]]
+import numpy as np
 
-            elif code == 99:
-                return(intcode[0])
+
+def run_intcode(intcode):
+    for i in range(0, len(intcode), 4):
+        code = intcode[i]
+        # Check operation type
+        if code == 1:
+            intcode[intcode[i+3]] = intcode[intcode[i+1]] + \
+                intcode[intcode[i+2]]
+        elif code == 2:
+            intcode[intcode[i+3]] = intcode[intcode[i+1]] * \
+                intcode[intcode[i+2]]
+
+        elif code == 99:
+            return(intcode[0])
 
 
 with open("Input2.txt", "r") as f:
-    intcode = [int(x) for x in f.readlines()[0].split(",")]
+    intcode_static = [int(x) for x in f.readlines()[0].split(",")]
 
-    intcode[1], intcode[2] = 12, 2
+    # Make a copy of intcode to be modified
+    intcode_part1 = np.copy(intcode_static)
+    intcode_part1[1], intcode_part1[2] = 12, 2  # Mofify noun and verb
 
-    print(run_intcode(intcode))  # Solution to part A
+    print(run_intcode(intcode_part1))  # Solution to part 1
 
-#print(run_intcode([1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]))
+    # Solution to part 2
+    for noun in range(100):
+        for verb in range(100):
+            # Make a copy of intcode to be modified
+            intcode_part2 = np.copy(intcode_static)
+            # Modify noun and verb
+            intcode_part2[1], intcode_part2[2] = noun, verb
+
+            if run_intcode(intcode_part2) == 19690720:
+                print(100 * noun + verb)
+                break
