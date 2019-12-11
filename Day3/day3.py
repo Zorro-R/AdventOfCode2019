aef2126a1ebad1c7f5640a25c9a5753db8634f0d
Jurij Waite <jurijlwaite@gmail.com>
2019-12-11 18:54:04 +0100

    Added solution to day 3 part 2

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,5 +1,8 @@
 with open("input3.txt", "r") as f:
     wire_instructions = f.readlines()
+    # wire_instructions = [
+    #     "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+    #     "U62,R66,U55,R34,D71,R55,D58,R83"]
 
     wires = []
     for wire_instruction in wire_instructions:
@@ -43,11 +46,23 @@ with open("input3.txt", "r") as f:
     def manhattan_distance(wire_coordinate):
         return abs(wire_coordinate[0]) + abs(wire_coordinate[1])
 
+    # Takes a wire and an intersection and returns the number of steps it takes that wire to get to that intersection
+    def steps_to_intersection(intersection, wires):
+        return wires[0].index(intersection) + wires[1].index(intersection)
+
     # Assume the closest intersection is infinitely far away
     closest_intersection = float("inf")
+    fastest_intersection = float("inf")
     for intersection in intersections:
         # If a closer intersection is found assume that to be the new closest intersection
-        if manhattan_distance(intersection) < closest_intersection:
-            closest_intersection = manhattan_distance(intersection)
+        dist_to_intersec = manhattan_distance(intersection)
+        if dist_to_intersec < closest_intersection:
+            closest_intersection = dist_to_intersec
+
+        # For part 2
+        fast_dist_to_intersec = steps_to_intersection(intersection, wires)
+        if fast_dist_to_intersec < fastest_intersection:
+            fastest_intersection = fast_dist_to_intersec
 
     print(closest_intersection)  # Solution to part 1
+    print(fastest_intersection)  # Solution to part 2
